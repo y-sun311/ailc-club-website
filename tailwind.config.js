744dc3e4d3c8d42259ea84f6966db0de1ae7854f
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-05-25 21:15:39 +1200

    Added FontAwesome package for icons.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
+        colors: {
+            'yellow-home': '#F4E0AD',
+          },
 
         fontFamily: {
             Source: ["Source Sans", "sans-serif"]
